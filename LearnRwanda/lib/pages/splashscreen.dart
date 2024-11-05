dd372abc04c23fe78b4cd9cbd5502fb2fb91536b
dntwaritag <d.ntwaritag@alustudent.com>
2024-11-05 22:19:19 +0200

    sscreen

---
+++ LearnRwanda/lib/pages/splashscreen.dart
@@ -0,0 +1,70 @@
+class LearningJourneyScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xFFF5F5F5),
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              // Replace 'assets/learning_journey_image.png' with the actual file name
+              Image.asset(
+                'assets/splash1',
+                height: 200,
+              ),
+              SizedBox(height: 20),
+              Text(
+                "Your learning journey is the reward",
+                style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                ),
+                textAlign: TextAlign.center,
+              ),
+              SizedBox(height: 10),
+              Text(
+                "Open your eyes, open your mind and seek, get to understand.",
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: 14,
+                  color: Colors.blue,
+                  decoration: TextDecoration.underline,
+                ),
+              ),
+              SizedBox(height: 20),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Icon(Icons.circle, size: 10, color: Colors.blue),
+                  SizedBox(width: 5),
+                  Icon(Icons.circle, size: 10, color: Colors.grey),
+                  SizedBox(width: 5),
+                  Icon(Icons.circle, size: 10, color: Colors.grey),
+                ],
+              ),
+              SizedBox(height: 20),
+              ElevatedButton(
+                onPressed: () {
+                  Navigator.pushNamed(context, '/establishConnections');
+                },
+                style: ElevatedButton.styleFrom(
+                  primary: Colors.blue,
+                  padding: EdgeInsets.symmetric(horizontal: 30, vertical: 15),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                ),
+                child: Text(
+                  "Next",
+                  style: TextStyle(fontSize: 16),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
