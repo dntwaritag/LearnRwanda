73518b92c2b64cfca08c3d6b6402d01a8386527b
dntwaritag <d.ntwaritag@alustudent.com>
2024-11-05 21:03:43 +0200

    splash

--- LearnRwanda/lib/main.dart
+++ LearnRwanda/lib/main.dart
@@ -1,41 +1,43 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/flutter_svg.dart';
 
-class AdaptiveELearningScreen extends StatelessWidget {
+class LearnRwandaSplashScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Color(0xFFF5F5F5), // Light background color
       body: Center(
-        child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 20),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              // Replace 'assets/adaptive_elearning_image.svg' with the actual file name
-              SvgPicture.asset(
-                'assets/adaptive_elearning_image.svg',
-                height: 200,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              "Splash Screen",
+              style: TextStyle(fontSize: 14, color: Colors.grey),
+            ),
+            SizedBox(height: 10),
+            // Replace 'assets/learnrwanda_logo.png' with the actual file name
+            Image.asset(
+              'assets/learnrwanda_logo.png',
+              height: 150,
+              width: 200,
+            ),
+            SizedBox(height: 20),
+            Text(
+              "LearnRwanda",
+              style: TextStyle(
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
               ),
-              SizedBox(height: 20),
-              Text(
-                "Adaptive E-Learning App",
-                style: TextStyle(
-                  fontSize: 24,
-                  fontWeight: FontWeight.bold,
-                ),
+            ),
+            SizedBox(height: 10),
+            Text(
+              "Your passion is waiting for your courage to catch up.",
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontSize: 16,
+                color: Colors.grey[700],
               ),
-              SizedBox(height: 10),
-              Text(
-                "Motivation is what sets you in motion, habit is what keeps you going.",
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                  fontSize: 16,
-                  color: Colors.grey[700],
-                ),
-              ),
-            ],
-          ),
+            ),
+          ],
         ),
       ),
     );
