6a819d5190094865690da3874683bdc4c70e4294
dntwaritag <d.ntwaritag@alustudent.com>
2024-11-06 18:41:55 +0200

    main

--- LearnRwanda/lib/main.dart
+++ LearnRwanda/lib/main.dart
@@ -1,8 +1,7 @@
-// main.dart
 import 'package:flutter/material.dart';
-import 'pages/package:learnrwanda_app/SplashScreen.dart';
-import 'pages/package:learnrwanda_app/SplashScreen1.dart';
-import 'pages/package:learnrwanda_app/SplashScreen2.dart';
+import 'package:your_app_name/pages/Splashscreen.dart';
+import 'package:your_app_name/pages/Splashscreen1.dart';
+import 'package:your_app_name/pages/Splashscreen2.dart';
 
 void main() {
   runApp(MyApp());
@@ -12,16 +11,15 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'LearnRwanda App',
+      title: 'LearnRwanda',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      // Set LearnRwandaSplashScreen as the initial route
-      initialRoute: '/',
+      home: Splashscreen(), // Set the first screen to load here
       routes: {
-        '/': (context) => LearnRwandaSplashScreen(),
-        '/learningJourney': (context) => LearningJourneyScreen(),
-        '/establishConnections': (context) => EstablishConnectionsScreen(),
+        '/Splashscreen': (context) => Splashscreen(),
+        '/Splashscreen1': (context) => Splashscreen1(),
+        '/Splashscreen2': (context) => Splashscreen2(),
       },
     );
   }
